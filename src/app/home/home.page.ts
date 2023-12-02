c6527a1ebac66a3d7f5534c61e7920aaff5785b4
Yohan491 <melo.yohan@outlook.com>
2023-12-01 21:37:34 -0300

    total

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,7 +5,7 @@ import { IonHeader, IonToolbar, IonTitle, IonContent,
 import { ItemCompra } from '../model/item-compra';
 import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { checkmarkOutline, closeOutline } from 'ionicons/icons';
+import { checkmarkOutline, closeOutline, trashBin } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 
 @Component({
@@ -24,24 +24,49 @@ export class HomePage {
 
   novoItem: ItemCompra = new ItemCompra();
 
+  total: number = 0;
+
+  proximoid: number = 1;
+
   constructor() {
-    addIcons({ checkmarkOutline, closeOutline });
+    addIcons({ checkmarkOutline, closeOutline, trashBin });
   }
 
   cadastrar() {
+    this.novoItem.id = this.proximoid++;
     this.lista.push(this.novoItem);
+
     this.novoItem = new ItemCompra();
   }
 
   toggleConcluido(i: ItemCompra) {
     i.compraRealizada = !i.compraRealizada;
+    this.totalmente();
+  
   }
 
+  totalmente(){
+    this.total=0;
+    for (let i of this.lista){
+      if(i.compraRealizada){
+        if(i.precoUnitario !=null){
+        this.total+= i.precoUnitario;
+        }
+      }
+    }
+  }
+  
   deletar(i :ItemCompra){
     for(var x = 0 ; x<this.lista.length; x++){
+      console.log(x);
+      console.log(this.lista[x].id);
+      console.log(i.id);
      if (this.lista[x].id == i.id){
        this.lista.splice(x,1);
+       this.totalmente();
        break;
+       
+
      }
     }
   }
